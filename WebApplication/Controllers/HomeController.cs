e406a9c8816b7cc683900a28767618e0e359346e
Madhu <bmadhusoothanan@20cube.com>
2019-01-17 15:31:36 +0530

    Test
    Test

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace WebApplication.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Welcome to 20cube data Madhu";
+            ViewBag.Message = "Welcome";
 
             return View();
         }
