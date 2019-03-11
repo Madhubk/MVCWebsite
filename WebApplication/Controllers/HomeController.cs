c57c452ab7d1563079ea8e6a835051f59fc72ba3
Madhu <bmadhusoothanan@20cube.com>
2019-03-11 15:51:19 +0530

    Again update homecontroller

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Madhu user 20cube";
+            ViewBag.Message = "Madhu user and job Accounting Team";
 
             return View();
         }
